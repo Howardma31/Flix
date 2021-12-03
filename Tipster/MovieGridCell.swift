60ae6da5dc0f027a914598e505a04482d421647c
Howard <howard.ma31@gmail.com>
2021-12-02 23:50:03 -0800

    Second Story

---
+++ Tipster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Tipster
+//
+//  Created by Howard Ma on 12/2/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
